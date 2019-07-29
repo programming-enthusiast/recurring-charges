808a453e66a2d595de70245d8f817d128ca9ff67
Henry Firth <firthh@firthh-00185.blc-hq.com>
2019-07-29 10:54:32 -0500

    Initial commit of elixir version of the challenge

---
+++ elixir/lib/recurring_charges.ex
@@ -0,0 +1,18 @@
+defmodule RecurringCharges do
+  @moduledoc """
+  Documentation for RecurringCharges.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RecurringCharges.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/test/recurring_charges_test.exs
@@ -0,0 +1,8 @@
+defmodule RecurringChargesTest do
+  use ExUnit.Case
+  doctest RecurringCharges
+
+  test "greets the world" do
+    assert RecurringCharges.hello() == :world
+  end
+end
