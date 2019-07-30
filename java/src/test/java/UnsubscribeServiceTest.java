4b70f9971043861a988334d1c7f035f3b8ef107a
Tom Patterson <tom@nextcapital.com>
2019-07-30 15:45:39 -0400

    Add Java Mocking

--- java/src/test/java/UnsubscribeServiceTest.java
+++ java/src/test/java/UnsubscribeServiceTest.java
@@ -1,28 +1,52 @@
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
 
-// You can run individual tests using the following command:
-// `gradle test --tests UnsubscribeServiceTest.testDoesNotIncludeChargesThatOnlyOccurOnce`
+
 public class UnsubscribeServiceTest {
+
+    private UnsubscribeService service;
+    List<HashMap<String, String>> statementCharges;
+
+    @Before
+    public void beforeFunction() {
+        service = mock(UnsubscribeService.class);
+        statementCharges = new ArrayList<HashMap<String, String>>();
+
+        when(service.displayRecurringCharges()).thenCallRealMethod();
+        when(service.getCharges()).thenReturn(statementCharges);
+    }
+
     @Test
     public void testDoesNotIncludeChargesThatOnlyOccurOnce() {
-        UnsubscribeService service = new UnsubscribeService();
+        addCharge("Spotify", "1/1/2019", "9.99");
 
         assertFalse(service.displayRecurringCharges().containsKey("Spotify"));
     }
 
     @Test
     public void testSumsTheChargesThatOccurMoreThanOnce() {
-        UnsubscribeService service = new UnsubscribeService();
+        addCharge("Spotify", "1/1/2019", "9.99");
+        addCharge("Spotify", "2/1/2019", "9.99");
 
         assertTrue(2 == service.displayRecurringCharges().get("Spotify"));
+    }
+
+    private void addCharge(String name, String date, String amount) {
+        HashMap<String, String> statementCharge = new HashMap<String, String>();
+        statementCharge.put("name", name);
+        statementCharge.put("date", date);
+        statementCharge.put("amount", amount);
+        statementCharges.add(statementCharge);
     }
 }
