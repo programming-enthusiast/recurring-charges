b7ca57dc9693ef67636103a4e8b775ac089f2820
Henry Firth <firthh@firthh-00185.blc-hq.com>
2019-07-31 11:21:48 -0500

    Abstract charge accumulation logic

--- elixir/lib/unsubscribe_service.ex
+++ elixir/lib/unsubscribe_service.ex
@@ -1,8 +1,13 @@
 defmodule UnsubscribeService do
   def display_recurring_charges(statements) do
     statements
-    |> Enum.reduce(%{}, fn charge, acc -> Map.update(acc, charge[:name], 1, &(&1 + 1)) end)
+    |> Enum.reduce(%{}, &increment_charge_count/2)
     |> Enum.filter(fn {_charge_name, charge_count} -> charge_count >= 1 end)
     |> Enum.into(%{})
+  end
+
+  defp increment_charge_count(%{name: charge_name}, charges) do
+    charge_count = charges[charge_name] || 0
+    Map.put(charges, charge_name, charge_count + 1)
   end
 end
